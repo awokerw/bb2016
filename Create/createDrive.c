1410e041910413eb23eac010bd7084611ccb2536
Alex Wong <hwong@Jeans-Air.gateway.sonic.net>
2016-02-26 20:13:24 -0800

    added initial create libraries

---
+++ Create/createDrive.c
@@ -0,0 +1,1 @@
+#include "createDrive.h"
---
+++ Create/template.h
@@ -0,0 +1,12 @@
+#define CREATE
+//#define LINK
+
+#include <stdio.h>
+
+#ifdef CREATE
+	#include "./createDrive.h"
+#endif
+
+#ifdef LINK
+	#include "./drive.h"
+#endif
