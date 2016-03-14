6bbcca0b4b3992a449f36da454bcfd7f9d275cd3
Alex Wong <hwong@Jeans-Air.gateway.sonic.net>
2016-03-13 20:27:24 -0700

    deleted kissproj dumb stuff, added SSHTransfer file

---
+++ SSHTransfer.sh
@@ -0,0 +1,4 @@
+scp -r root@192.168.124.1:~/Documents/KISS/src/Wallaby ~/Desktop
+scp -r root@192.168.124.1:~/Documents/KISS/include/Wallaby ~/Desktop
+scp -r root@192.168.124.1:~/Documents/KISS/src/Create ~/Desktop
+scp -r root@192.168.124.1:~/Documents/KISS/include/Create ~/Desktop
